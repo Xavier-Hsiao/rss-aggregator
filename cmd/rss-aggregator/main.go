5d3a4e1bb5eab68d48713020cfdb335da0148c6e
Xavier <tim88776@gmail.com>
2024-10-06 22:50:27 +0800

    feat: add initial command structure & implement login command

--- cmd/rss-aggregator/main.go
+++ cmd/rss-aggregator/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"log"
+	"os"
 
+	"github.com/Xavier-Hsiao/rss-aggregator/internal/app"
+	"github.com/Xavier-Hsiao/rss-aggregator/internal/cli"
 	"github.com/Xavier-Hsiao/rss-aggregator/internal/config"
 )
 
@@ -14,14 +16,26 @@ func main() {
 		log.Fatalf("failed to read config file")
 	}
 
-	currentUserName := "Xavier"
-
-	c.SetUser(currentUserName)
-
-	c, err = config.Read()
-	if err != nil {
-		log.Fatalf("failed to read config file")
+	state := &app.State{
+		Config: &c,
 	}
 
-	fmt.Printf("Config content: %v, %v\n", c.DbURL, c.CurrentUserName)
+	commands := &cli.Commands{
+		Handlers: make(map[string]func(*app.State, cli.Command) error),
+	}
+	commands.Register("login", cli.HandlerLogin)
+
+	args := os.Args
+	if len(args) < 2 {
+		log.Fatal("not enough argument provided")
+	}
+
+	command := &cli.Command{
+		Name: args[1],
+		Args: args[2:],
+	}
+	err = commands.Run(state, *command)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
