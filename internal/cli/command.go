38d81387de87c1b6e0b5bdc0ee58f6c4febd9b11
Xavier <tim88776@gmail.com>
2024-10-06 21:14:46 +0800

    feat: add login handler function

---
+++ internal/cli/command.go
@@ -0,0 +1,6 @@
+package cli
+
+type Command struct {
+	name string
+	args []string
+}
---
+++ internal/cli/handler_login.go
@@ -0,0 +1,21 @@
+package cli
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/Xavier-Hsiao/rss-aggregator/internal/app"
+)
+
+func HandlerLogin(s *app.State, cmd Command) error {
+	if len(cmd.args) == 0 {
+		return errors.New("Error: expect one argument")
+	}
+
+	currentUserName := s.Config.CurrentUserName
+	s.Config.SetUser(currentUserName)
+
+	fmt.Println("The user name has been set.")
+
+	return nil
+}
