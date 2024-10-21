a62b60ffd4e6390d57bf85cb01e002dcd3c26be1
Xavier <tim88776@gmail.com>
2024-10-21 12:19:16 +0800

    feat: save posts to db using agg command

--- internal/cli/handler_agg.go
+++ internal/cli/handler_agg.go
@@ -2,11 +2,15 @@ package cli
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
 	"log"
 	"time"
 
 	"github.com/Xavier-Hsiao/rss-aggregator/internal/app"
+	"github.com/Xavier-Hsiao/rss-aggregator/internal/datbase"
+	"github.com/google/uuid"
+	"github.com/lib/pq"
 )
 
 func HandlerAgg(s *app.State, cmd Command) error {
@@ -51,9 +55,36 @@ func scrapeFeeds(s *app.State) {
 		return
 	}
 
-	// Iterate over the items in the feed and print their titles
-	for i, item := range feedData.Channel.Item {
-		fmt.Printf("#%d - %s\n", i, item.Title)
+	// Iterate over the items in the feed and save posts to database
+	for _, item := range feedData.Channel.Item {
+		// Convert published_at to time
+		publishedAt, err := time.Parse(time.RFC1123Z, item.PubDate)
+		if err != nil {
+			log.Printf("Failed to convert published_at time: %v", err)
+		}
+
+		_, err = s.DB.CreatePost(ctx, datbase.CreatePostParams{
+			ID:        uuid.New(),
+			CreatedAt: time.Now().UTC(),
+			UpdatedAt: time.Now().UTC(),
+			Title:     item.Title,
+			Description: sql.NullString{
+				String: item.Description,
+				Valid:  true,
+			},
+			PublishedAt: publishedAt,
+			FeedID:      feed.ID,
+		})
+		if err != nil {
+			// Skip duplicate URL error
+			if pqErr, ok := err.(*pq.Error); ok {
+				if pqErr.Code == "23505" {
+					continue
+				}
+			}
+			log.Printf("Failed to create post: %v", err)
+			continue
+		}
 	}
 	fmt.Printf("Feed %s collected: %d posts found!\n", feed.Name, len(feedData.Channel.Item))
 }
